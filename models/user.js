2a8c14f47af87ec2a321397695bf645969440375
mliq <liquori@gmail.com>
2015-05-16 20:45:09 -0500

    Regular mode assignment complete

--- models/user.js
+++ models/user.js
@@ -5,7 +5,10 @@ var mongoose = require('mongoose'),
 
 var UserSchema = new Schema({
     username: { type: String, required: true, index: { unique: true } },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    firstname: String,
+    lastname: String,
+    email: String
 });
 
 // Hash passwords before user documents are saved to MongoDB
